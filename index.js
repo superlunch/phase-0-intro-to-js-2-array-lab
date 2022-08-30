d46ce497f3613825549cd9e2292652c5326824a5
Scout Cox <coxscout@gmail.com>
2022-08-30 11:05:05 -0400

    commit tree 123cbab0bac8c662be8df30bf57f5a7cab39582f

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+       
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(cats);
+    return cats;
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+   const newCats = cats.slice(0, cats.length-1);
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(-2);
+    return newCats;
+}
